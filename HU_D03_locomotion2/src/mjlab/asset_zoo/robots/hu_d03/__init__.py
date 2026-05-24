d6be620b3b2297e747a46b440bba2a6bd29f2558
Khanh Pham <email-cua-ban@example.com>
2026-05-24 22:03:29 +0700

    Remove locomotion2 and locomotion3 folders

--- HU_D03_locomotion2/src/mjlab/asset_zoo/robots/hu_d03/__init__.py
+++
@@ -1,8 +0,0 @@
-"""HU_D03 robot assets and configuration."""
-
-from mjlab.asset_zoo.robots.hu_d03.hu_d03_constants import (
-  HU_D03_ACTION_SCALE as HU_D03_ACTION_SCALE,
-)
-from mjlab.asset_zoo.robots.hu_d03.hu_d03_constants import (
-  get_hu_d03_robot_cfg as get_hu_d03_robot_cfg,
-)
