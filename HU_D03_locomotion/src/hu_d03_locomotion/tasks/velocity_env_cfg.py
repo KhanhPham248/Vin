337158cb71caeccbbfc851aa6a6a893d6b42bda6
Khanh Pham <email-cua-ban@example.com>
2026-05-25 12:17:03 +0700

    feat: import TerminationTermCfg in velocity_env_cfg.py

--- HU_D03_locomotion/src/hu_d03_locomotion/tasks/velocity_env_cfg.py
+++ HU_D03_locomotion/src/hu_d03_locomotion/tasks/velocity_env_cfg.py
@@ -15,6 +15,7 @@ from mjlab.managers.event_manager import EventTermCfg
 from mjlab.managers.reward_manager import RewardTermCfg
 from mjlab.managers.curriculum_manager import CurriculumTermCfg
 from mjlab.managers.scene_entity_config import SceneEntityCfg
+from mjlab.managers.termination_manager import TerminationTermCfg
 from mjlab.sensor import (
     ContactMatch,
     ContactSensorCfg,
